396006f7e5fc6f28d05e732dd3b83f3b5a188ffa
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-03-31 19:47:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <html style="">
-<head><link rel="stylesheet" href="typography.css" type="text/css"></head>
+<head><link rel="stylesheet" href="styles.css" type="text/css"></head>
 <h1>Poly Nucleus</h1>
 </html>
