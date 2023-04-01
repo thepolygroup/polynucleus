9b7b61c5f98a86694816301ee4dd5ad5b42d4904
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-02 01:07:51 +0200

    HTML layout

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<html style="">
-<head><link rel="stylesheet" href="styles.css" type="text/css"></head>
-<h1>Poly Nucleus</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Poly Nucleus</title>
+
+        <!-- META General -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta property="og:type" content="website"/>
+        <meta name="author"  content="The Poly Group"/>
+
+        <!-- FAVICON -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7537f8">
+        <meta name="msapplication-TileColor" content="#7537f8">
+        <meta name="theme-color" content="#7537f8">
+
+        <!-- CSS General -->
+        <link rel="stylesheet" href="styles.css" type="text/css">
+
+        <!-- FONTS -->
+
+        <!-- PAGE-SPECIFIC META start -->
+        <meta property="og:title" content="Poly Nucleus"/>
+        <meta property="og:url" content="https://polynucleus.com"/>
+        <meta name="keywords" content="Design system" />
+        <meta name="description" property="og:description" content="An open source design system." />
+        <meta property="og:image" content=""/>
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="Poly Nucleus" />
+        <meta name="twitter:creator" content="@ThePolyGroupCo">
+        <meta name="twitter:title" content="Poly Nucleus" />
+        <meta name="twitter:description" content="An open source design system." />
+        <meta name="twitter:image" content="" />
+        <meta name="robots" content="index, follow">
+        <!-- PAGE-SPECIFIC META end -->
+    </head>
+    <body class="light-theme">
+        <h1>Poly Nucleus</h1>
+    </body>
 </html>
