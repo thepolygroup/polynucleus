3bd95ce50acfb3e51675a30ffe1d2a6b2e09bdcd
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-02 01:34:12 +0200

    Decreased letterspacing on title

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
         <meta name="twitter:image" content="" />
         <meta name="robots" content="index, follow">
         <!-- PAGE-SPECIFIC META end -->
-        
+
     </head>
     <body class="light-theme">
         <header style="padding: 10px;">
             <img src="imgs/polynucleus.svg" height="50px" width="auto">
         </header>
-        <h1 class="title-1 align-center" style="max-width:1200px;margin:0 auto;">The open-source design system</h1>
+        <h1 class="title-1 align-center" style="max-width:1200px;margin:0 auto;letter-spacing: -0.5px;">The open-source design system</h1>
     </body>
 </html>
