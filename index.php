1b586362b09337da39c61197343aa42d964f4703
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-02 01:27:02 +0200

    Separated head content in includes.php

--- index.php
+++ index.php
@@ -1,31 +1,13 @@
+<?php
+    include 'includes.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Poly Nucleus</title>
 
-        <!-- META General -->
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta property="og:type" content="website"/>
-        <meta name="author"  content="The Poly Group"/>
-
-        <!-- FAVICON -->
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7537f8">
-        <meta name="msapplication-TileColor" content="#7537f8">
-        <meta name="theme-color" content="#7537f8">
-
-        <!-- CSS General -->
-        <link rel="stylesheet" href="styles.css" type="text/css">
-
-        <!-- FONTS -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://polynucleus.com/styles.css" type="text/css">
+        <?php echo head() ?>
+        
 
         <!-- PAGE-SPECIFIC META start -->
         <meta property="og:title" content="Poly Nucleus"/>
@@ -48,6 +30,6 @@
         <header style="padding: 10px;">
             <img src="imgs/polynucleus.svg" height="50px" width="auto">
         </header>
-        <h1 class="title-1 align-center" style="max-width:900px;margin:0 auto;">The open-source design system</h1>
+        <h1 class="title-1 align-center" style="max-width:1200px;margin:0 auto;">The open-source design system</h1>
     </body>
 </html>
